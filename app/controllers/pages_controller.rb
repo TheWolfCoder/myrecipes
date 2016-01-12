9ea12b229cecb247247aee5b1d1618df3465485d
thewolfcoder <stephan.r.brown@gmail.com>
2016-01-12 08:36:09 +0000

    Create chefs resources and add authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,6 @@ class PagesController < ApplicationController
   end
   
   def home
-    
+    redirect_to recipes_path if logged_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
       post 'like'
     end
   end
-
   
-
+  resources :chefs, except: [:new]
+  
+  get '/register', to: 'chefs#new'
+  
+  get '/login', to: "logins#new"
+  post '/login', to: "logins#create"
+  get '/logout', to: "logins#destroy"
+  
+ # login -> new session
+  #logout -> close session
+  #post login -> create session
+  
 end
